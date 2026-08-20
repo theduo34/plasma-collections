de8c89b602b5e9a4a534c1163361c797cef49335
Emmanuel <emmsom506@gmail.com>
2026-08-20 04:35:46 +0000

    Allow CSS transitions on theme change (needed for the new toggle animation)

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem={false}>
       {children}
     </NextThemesProvider>
   )
